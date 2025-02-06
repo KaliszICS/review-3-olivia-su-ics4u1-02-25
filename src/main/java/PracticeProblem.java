25170a3ff0a95b03d2bc1fe9788d938006e002e2
olivia-su-ics4u1-02-25 <olivia.su@student.tdsb.on.ca>
2025-02-06 15:29:42 +0000

    YOOO I DONE

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -13,38 +13,87 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		Scanner sc = new Scanner(System.in);
+		System.out.print("In: ");
+
+		String word = sc.nextLine();
+		System.out.println(word.charAt(0));
 		
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		Scanner sc = new Scanner(System.in);
+		System.out.print("In: ");
+
+		boolean trueFalse = sc.nextBoolean();
+		sc.nextLine();
+
+		System.out.println(!trueFalse);
 		
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		Scanner sc = new Scanner(System.in);
+		System.out.print("In: ");
+
+		int num = sc.nextInt();
+		sc.nextLine();
+
+		System.out.println(num > 5);
 		
 	}
 
 	public static void q4() {
-		//Write question 4 code here
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("In: ");
+
+		double num = sc.nextDouble();
+		sc.nextLine();
+		System.out.println((num>=-2)&&(num<=2));
 	}
 
 	public static void q5() {
-		//Write question 5 code here
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("In: ");
+
+		String word = sc.nextLine();
+
+		System.out.println(word.equals("Hello World"));
+	
 	}
 
 	public static void q6() {
-		//Write question 6 code here
+		Scanner sc = new Scanner(System.in);
+		System.out.print("In: ");
 		
+		int num = sc.nextInt();
+		sc.nextLine();
+
+		System.out.print("In: ");
+
+		int num2 = sc.nextInt();
+		sc.nextLine();
+
+		System.out.println(num <= num2);
+
 	}
 
 	public static void q7() {
-		//Write question 7 code here
+		Scanner sc = new Scanner(System.in);
+		System.out.print("In: ");
+		
+		double num = sc.nextDouble();
+		sc.nextLine();
+
+		System.out.print("In: ");
+		
+		double num2 = sc.nextDouble();
+		sc.nextLine();
+
+		System.out.println(num > num2);
 		
 	}
 
 }
+
+//Hi Mr.K I hate the test because why I cannot have my ln :<
